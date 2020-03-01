ecc508d0da1f44d19943d7e03a4e1fb1ad5f375d
Sharjeel Yunus <61178058+SharjeelDev@users.noreply.github.com>
2020-03-01 17:46:12 +0500

    Update

--- script.js
+++ script.js
@@ -2,6 +2,25 @@ var button = document.getElementById("add");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
 
+//from StackOverflow:
+function removeParent(evt) {
+  evt.target.removeEventListener("click", removeParent, false);
+  evt.target.parentNode.remove();
+}
+
+//click on a list item and it strikethroughs the text
+function getEventTarget(e){
+	e = e || window.event;
+	return e.target || e.srcElement;
+}
+
+ul.onclick = function(event){
+	var target = getEventTarget(event);
+  target.classList.toggle("done");
+  // var trash = getEventTarget(event);
+	// target.classList.toggle("trash");
+}
+
 function inputLength() {
   return input.value.length;
 }
