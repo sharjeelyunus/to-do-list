21c1873c012e50ef727906d4fa20b3f527f80db7
Sumit Upadhyay <skumarupadhyay1@gmail.com>
2023-10-11 17:33:05 +0530

    Users Todo doesn't persist after refresh : Fixed..

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var button = document.getElementById("add");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
-
+let todos = [];
 //from StackOverflow:
 function removeParent(evt) {
   evt.target.removeEventListener("click", removeParent, false);
@@ -25,12 +25,50 @@ function inputLength() {
   return input.value.length;
 }
 
+function loadTodoList() {
+  
+  const todos = JSON.parse(localStorage.getItem("Todos")) || [];
+
+  todos.forEach((todoText) => {
+    const li = document.createElement("li");
+    li.appendChild(document.createTextNode(todoText));
+    ul.appendChild(li);
+  });
+}
+
+// Call the function to load the list when the page loads.
+window.addEventListener("load", loadTodoList);
+
 function createListElement() {
+  const inputValue = input.value;
+  if (inputValue.trim() === "") {
+    return; // Do not add empty todos
+  }
+
+
+
+  // Retrieve existing todos from localStorage or create an empty array if it doesn't exist.
+  const existingTodos = JSON.parse(localStorage.getItem("Todos"));
+  if (Array.isArray(existingTodos)) {
+    todos = existingTodos;
+  }
+
+  // Add the new todo to the array.
+  todos.push(inputValue);
+
+  // Store the updated array in localStorage.
+  localStorage.setItem("Todos", JSON.stringify(todos));
+
+  // Create a list item for the new todo.
   var li = document.createElement("li");
-  li.appendChild(document.createTextNode(input.value));
+
+  li.appendChild(document.createTextNode(inputValue));
   ul.appendChild(li);
+
   input.value = "";
 }
+
+
 
 function addListAfterClick() {
   if (inputLength() > 0) {
